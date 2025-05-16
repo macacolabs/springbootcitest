2e0814d86c94748a0bc6fa02dc275a06dd421b9e
macacolabs <monkey.ohgiraffers@gmail.com>
2025-05-16 12:32:01 +0900

    first commit

---
+++ src/main/java/com/ohgiraffers/springbootnginxcircleci/SpringbootNginxCircleciApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.springbootnginxcircleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNginxCircleciApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootNginxCircleciApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohgiraffers/springbootnginxcircleci/SpringbootNginxCircleciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.springbootnginxcircleci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNginxCircleciApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
